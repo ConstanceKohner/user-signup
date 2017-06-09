ec72bb93bcd288a96c79bb1afce32850a255dd79
Constance <catubbs88@yahoo.com>
2017-06-09 11:23:59 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# ASSIGNMENT REQUIREMENTS
+# If the user's form submission is not valid, you should reject it and re-render the form with some feedback to inform the user of what they did wrong. The following things should trigger an error:
+#The user leaves any of the following fields empty: username, password, verify password.
+#The user's username or password is not valid -- for example, it contains a space character or it consists of less than 3 characters or more than 20 characters (e.g., a username or password of "me" would be invalid).
+#The user's password and password-confirmation do not match.
+#The user provides an email, but it's not a valid email. Note: the email field may be left empty, but if there is content in it, then it must be validated. The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
+#Each feedback message should be next to the field that it refers to.
+#For the username and email fields, you should preserve what the user typed, so they don't have to retype it. With the password fields, you should clear them, for security reasons.
+#If all the input is valid, then you should redirect the user to a welcome page that uses the username input to display a welcome message of: "Welcome, [username]!"
+#Use templates (one for the index/home page and one for the welcome page) to render the HTML for your web app.
+#While we've covered how to specify different input types than just text (e.g., password and email), and normally you will want to use these when they are appropriate, for this assignment do not use these. Instead, just use text. This will enable us to check that the server side validation is working by letting errors through the client side.
+
+from flask import Flask, request
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    template = jinja_env.get_template('index.html')
+    return template.render()
+
+@app.route("/welcome")
+def index():
+    template = jinja_env.get_template('welcome.html')
+    return template.render()
